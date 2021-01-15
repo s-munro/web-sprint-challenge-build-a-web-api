a6a79b5b2aba707e8381c0ed8b0ef26c5e4b119e
s-munro <68410363+s-munro@users.noreply.github.com>
2021-01-14 20:05:16 -0500

    validate project and validate action id middlewares set up

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,25 +1,36 @@
 // Write your "actions" router here!
 const express = require("express");
 
+const Actions = require("./actions-model");
+
+const { validateAction, validateActionId } = require("../middleware");
+
 const router = express.Router();
 
 router.get("/", (req, res) => {
+  Actions.get()
+    .then((actions) => {
+      res.status(200).json(actions);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err, message: "Internal server error" });
+    });
   // get code here
 });
 
-router.get("/:id", (req, res) => {
+router.get("/:id", validateActionId, (req, res) => {
   // get code here
 });
 
-router.post("/", (req, res) => {
+router.post("/", validateAction, (req, res) => {
   // get code here
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", validateActionId, validateAction, (req, res) => {
   // get code here
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", validateActionId, (req, res) => {
   // get code here
 });
 module.exports = router;
--- api/middleware/index.js
+++ api/middleware/index.js
@@ -1,4 +1,23 @@
+const Actions = require("../actions/actions-model");
+const Projects = require("../projects/projects-router");
+
 const validateActionId = (req, res, next) => {
+  const { id } = req.params;
+  Actions.get(id)
+    .then((action) => {
+      if (!action) {
+        res
+          .status(404)
+          .json({ message: `An action by ID ${id} does not exist.` });
+      } else {
+        req.action = action;
+        next();
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ message: "Internal server error" });
+    });
   // validate
 };
 
@@ -6,17 +25,31 @@ const validateAction = (req, res, next) => {
   // validate
 };
 
-const validatePostId = (req, res, next) => {
+const validateProjectId = (req, res, next) => {
+  const { id } = req.params;
+  Projects.get(id)
+    .then((project) => {
+      if (!project) {
+        res.status(404).json({ message: `Project by ID ${id} does not exist` });
+      } else {
+        req.project = project;
+        next();
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ message: "Error retrieving project" });
+    });
   // validate
 };
 
-const validatePost = (req, res, next) => {
+const validateProject = (req, res, next) => {
   // validate
 };
 
 module.exports = {
   validateActionId,
   validateAction,
-  validatePostId,
-  validatePost,
+  validateProjectId,
+  validateProject,
 };
