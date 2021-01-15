4ddb83ab2187a06a45daea52d261322987a569ad
s-munro <68410363+s-munro@users.noreply.github.com>
2021-01-14 19:27:21 -0500

    scaffolding

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,10 @@
 // Write your "actions" router here!
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  // get code here
+});
+
+module.exports = router;
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,10 @@
 // Write your "projects" router here!
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/", (res, req) => {
+  // get code
+});
+
+module.exports = router;
